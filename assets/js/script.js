80347fdc6552ae79ccb391cbc1c6d8b94cc86daa
If-Kaliel <kaliel.aquino1303@gmail.com>
2025-11-09 15:03:41 -0300

    feat: menu hamburguer para celular validado.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-document.addEventListener("DOMContentLoaded", function() {}
+document.addEventListener("DOMContentLoaded", function() {
+  const btn = document.querySelector(".hamburger");
+  const nav = document.querySelector('nav[role="navegação"]');
+  if (btn && nav) {
+    btn.addEventListener("click", function() {
+      if (nav.style.display === "flex") {
+        nav.style.display = "none";
+      } else {
+        nav.style.display = "flex";
+        nav.style.flexDirection = "column";
+        nav.style.alignItems = "center";
+        nav.style.gap = "10px";
+      }
+    });
+  }
+});
