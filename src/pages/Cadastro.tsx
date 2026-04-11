e9257ba810cf1a0fb9f5ba87d67317d88a7808cd
If-Kaliel <kaliel.aquino1303@gmail.com>
2026-04-11 11:04:15 -0300

    feat: Importando formulário.

--- src/pages/Cadastro.tsx
+++ src/pages/Cadastro.tsx
@@ -1,2 +1,10 @@
 // Sistema TDB - Integrando Java e database. Aqui ficará o setup inicial preparando o terreno para a sprint 4. Na sprint 4 integraremos o backend com o front.
 
+import { useForm } from 'react-hook-form';
+
+interface CadastroFormData {
+  nome: string;
+  cpf: string;
+  dataNasc: string;
+  endereco: string;
+}
